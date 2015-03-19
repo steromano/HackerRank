eafec43134998516c7eb03f6e1025455008b26ab
Stefano Romano <stefano.romano@gamesys.co.uk>
2015-03-19 13:53:15 +0000

    minor reorganisation and comments

--- QuoraAnswerClassifier/quora_classifier.py
+++ QuoraAnswerClassifier/quora_classifier.py
@@ -3,17 +3,18 @@ import fileinput
 from sklearn.ensemble import RandomForestClassifier
 
 ## Read and parse input
-inputf = fileinput.input()
-n, m = map(int, inputf.next().split())
-id_train = []
-x_train = np.zeros((n, m))
-y_train = np.zeros(n) 
 def parse_record(line, labeled = True):
     rec_id = line[0]
     rec_y = line[1] if labeled else None
     k = 2 if labeled else 1
     rec_x = map(lambda x: x.split(":")[1], line[k:])
     return rec_id, rec_x, rec_y
+
+inputf = fileinput.input()
+n, m = map(int, inputf.next().split())
+id_train = []
+x_train = np.zeros((n, m))
+y_train = np.zeros(n) 
 for i in range(n):
     rec_id, rec_x, rec_y = parse_record(inputf.next().split())
     id_train.append(rec_id)
@@ -28,15 +29,16 @@ for i in range(q):
     x_test[i, :] = rec_x
         
 
-## Train RandomForest
+## Train a small RandomForest
 rf = RandomForestClassifier(n_estimators = 100,
-                            oob_score = True,
                             verbose = 0,
                             n_jobs = -1)
-
 rf.fit(x_train, y_train)
+
+## Predict the test set
 y_test = zip(id_test, 
              map(lambda y: "+1" if y == 1.0 else "-1", rf.predict(x_test)))
 
+## Output predictions
 for y_id, y in y_test:
     print y_id, y
